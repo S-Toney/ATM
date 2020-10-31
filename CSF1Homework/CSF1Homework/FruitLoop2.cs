2d1176b4b02d918a3ae5efa2d90dd6337fd77a44
Stacy Toney <stacy.toney@protonmail.com>
2020-10-30 23:43:58 -0500

    FruitLoop2 class and program created

---
+++ CSF1Homework/CSF1Homework/FruitLoop2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop2
+    {
+        static void Main()
+        {
+            Console.Title = "Fruit Loop 2";
+            string[] fruit = new string[] { "apple", "orange", "banana", "plum", "peach", "mango" };
+
+            for (int fruitC = 0; fruitC < fruit.Length; fruitC++)
+            {
+                Console.WriteLine(fruit[fruitC]);
+                Console.WriteLine(new string ('.', fruitC ));
+            }
+        }
+    }
+}
