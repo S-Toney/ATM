26976d571479c2c064e6fca5a18c111c76952060
Stacy Toney <stacy.toney@protonmail.com>
2020-10-30 23:08:02 -0500

    Renamed class and wrote program FruitLoop1

---
+++ CSF1Homework/CSF1Homework/FruitLoop1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop1
+    {
+        static void Main(string[] args)
+        {
+            string[] fruit = new string[] { "banana", "apple", "pear", "plum", "cherry", "orange" };
+            for (int fruitC = 0; fruitC < fruit.Length; fruitC++)
+            {
+                Console.WriteLine(fruit[fruitC]);
+            }
+
+        }
+    }
+}
