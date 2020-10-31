12bf322892b8d30e98fe9466921c7ee68f2fd3a2
Stacy Toney <stacy.toney@protonmail.com>
2020-10-30 23:09:43 -0500

    Console title changed

--- CSF1Homework/CSF1Homework/FruitLoop1.cs
+++ CSF1Homework/CSF1Homework/FruitLoop1.cs
@@ -10,6 +10,7 @@ namespace CSF1Homework
     {
         static void Main(string[] args)
         {
+            Console.Title = "Fruit Loop 1";
             string[] fruit = new string[] { "banana", "apple", "pear", "plum", "cherry", "orange" };
             for (int fruitC = 0; fruitC < fruit.Length; fruitC++)
             {
