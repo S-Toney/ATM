be5b642647fb63873d14d2f2b6c69d7ee447c465
Stacy Toney <stacy.toney@protonmail.com>
2020-10-31 19:17:58 -0500

    Login class created and coded

---
+++ CSF1Homework/CSF1Homework/Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Login
+    {
+        static void Main()
+        {
+            Console.Title = "Login";
+            string userName = "ADMIN";
+            string password = "Admin";
+            string input;
+            string pass;
+            int chances = 0;
+            do
+            {
+                Console.WriteLine("Enter your username: ");
+                input = Console.ReadLine().ToUpper();
+                chances++;
+            } while (input != userName && chances < 3);
+            if (input == userName)
+            {
+                Console.WriteLine($"WELCOME {userName}");
+                chances = 0;
+                do
+                {
+                    Console.WriteLine("Enter your password: ");
+                    pass = Console.ReadLine();
+                    chances++;
+                } while (pass != password && chances < 3);
+                if (pass == password)
+                {
+                    Console.WriteLine("ACCESS GRANTED");
+                }
+                else
+                {
+                    Console.WriteLine("ACCESS DENIED");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ACCESS DENIED");
+            }
+
+        }
+    }
+}
